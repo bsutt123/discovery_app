c527113f3b159035dd5c42bbc00dadc714bac571
Brady Sutton <bsutt123@gmail.com>
2018-02-23 13:15:54 -0500

    add in routes for themoviedb api calls

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  #routes that connect the api to popular get requests from The Movie DB
+  get '/movies/popular', to: 'movies#popular'
+  get '/movies/top_rated', to: 'movies#top_rated'
+  get '/movies/upcoming', to: 'movies#upcoming'
+
+  #create a route for the search parameter
+  get '/movies/search', to: 'movies#search'
 end
