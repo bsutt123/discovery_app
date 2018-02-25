e58d89ba80bc7278823fd278f43cfe5d75f186bd
Brady Sutton <bsutt123@gmail.com>
2018-02-24 22:03:11 -0500

    create home page javascript and css

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,5 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
+import styles from './home.module.css'
+
+//import components
+import Hero from 'Components/Hero/Hero';
+
+//import images
+import HeroImage from 'Images/home/hero.jpg'
 
 
-class Home extends Component {
+const Home = (props) => {
+  return (
+    <div className={styles.container}>
+      <Hero image={HeroImage}>
+        <h1 className={styles.heroMainHeading} > Discover your next show </h1>
+        <h3 className={styles.heroSubHeading} > Browse ... </h3>
+        <h3 className={styles.heroSubHeading} > Binge ... </h3>
+        <h3 className={styles.heroSubHeading} > Repeat </h3>
+      </Hero>
+    </div>
+  )
 }
+
+export default Home;
