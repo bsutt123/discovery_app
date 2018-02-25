5fb28f3e5a9901bb7835fa90d8f5728f4ff1ce67
Brady Sutton <bsutt123@gmail.com>
2018-02-24 22:03:50 -0500

    add home page to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import logo from './logo.svg';
 import './App.css';
 
 import Navbar from 'Components/Navbar/Navbar';
+import Home from 'Pages/Home/Home';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-       <Navbar />
+        <Navbar />
+        <Home />
       </div>
     );
   }
