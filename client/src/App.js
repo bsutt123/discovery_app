20e9eb1623c7fb3200f1feda40ab47d3b8f19995
Brady Sutton <bsutt123@gmail.com>
2018-02-25 10:46:14 -0500

    implement react-router and redux in application

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Provider } from 'react-redux';
 import './App.css';
 
 import Navbar from 'Components/Navbar/Navbar';
 import Home from 'Pages/Home/Home';
+import VisibleMovies from 'Containers/VisibleMovies/VisibleMovies';
+import TvShows from 'Pages/TvShows/TvShows';
+
+import store from './store';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Navbar />
-        <Home />
+        <Provider store={store} >
+          <div>
+            <Navbar />
+            <Router>
+              <div>
+                <Route exact path="/" component={Home} />
+                <Route path="/movies" component={VisibleMovies} />
+              </div>
+            </Router>
+          </div>
+        </Provider>
       </div>
     );
   }
