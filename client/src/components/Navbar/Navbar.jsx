43635d0a7dc5318d30766cefb5585be1e49247ae
Brady Sutton <bsutt123@gmail.com>
2018-02-25 12:05:19 -0500

    Use Link inside Navbar to route

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -3,15 +3,15 @@ import styles from './navbar.module.css';
 
 import Logo from 'Images/discovr.png';
 
-const Navbar = (props) => {
+const Navbar = ({Link}) => {
   return (
     <div className={styles.container} >
       <div className={styles.logoBox}>
-        <img src={Logo} className={styles.logo}/>
+        <Link to="/"> <img src={Logo} className={styles.logo}/> </Link>
       </div>
       <div className={styles.navList} >
-        <span className={styles.navItem}> Movies </span>
-        <span className={styles.navItem}> Television </span>
+        <Link to="/movies"> <span className={styles.navItem}> Movies </span> </Link>
+        <Link to="/tv"> <span className={styles.navItem}> Television </span> </Link>
       </div>
     </div>
   )
