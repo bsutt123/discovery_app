6fef8a4e4122cda809f5f44ce2afd4fea82d7b80
Brady Sutton <bsutt123@gmail.com>
2018-02-26 11:50:10 -0500

    change moviesReducer to only control movies array

--- client/src/reducers/moviesReducer.js
+++ client/src/reducers/moviesReducer.js
@@ -1,24 +1,12 @@
 import { SET_MOVIES_FILTER, SEARCH_MOVIES } from 'Actions/movieActions'
 
-const initialState = {
-  displaySearch:false,
-  movies: []
-}
 
-export function moviesReducer(state = initialState, action) {
+export function moviesReducer(state = [], action) {
   switch(action.type) {
     case SET_MOVIES_FILTER:
-      return {
-        ...state,
-        displaySearch: false,
-        movies: action.movies
-      }
+      return action.movies
     case SEARCH_MOVIES:
-      return {
-        ...state,
-        displaySearch: true,
-        movies: action.movies
-      }
+      return action.movies
     default:
       return state
   }
