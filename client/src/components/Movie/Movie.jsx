d72f8168441c200ffdf3449106d8c59af3744b24
Brady Sutton <bsutt123@gmail.com>
2018-02-28 12:24:10 -0500

    make the image for movie details into Router Link

--- client/src/components/Movie/Movie.jsx
+++ client/src/components/Movie/Movie.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './movie.module.css';
+import {Link} from 'react-router-dom';
 
 const Movie = ({ movie }) => {
   const prePosterPath = 'https://image.tmdb.org/t/p/w500'
   return (
     <div className={styles.container} >
       <div className={styles.imageBox} >
-        <img src={`${prePosterPath}${movie.poster_path}`} alt={`poster for ${movie.title}`}className={styles.image} />
+        <Link to={`/movies/${movie.id}`}> <img src={`${prePosterPath}${movie.poster_path}`} alt={`poster for ${movie.title}`}className={styles.image} /> </Link>
       </div>
       <div className={styles.contentBox} >
         <h1 className={styles.title} > { movie.title } </h1>
