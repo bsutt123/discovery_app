fcc55c7b93c2bed3878cfdff9b491fed38fce256
Brady Sutton <bsutt123@gmail.com>
2018-02-27 22:49:09 -0500

    create the Tv list presentor

---
+++ client/src/components/TvList/TvList.jsx
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+
+import TvShow from 'Components/TvShow/TvShow';
+
+import styles from './tv-list.module.css';
+
+
+class TvList extends Component {
+  componentDidMount() {
+    this.props.setFilter("popular");
+  }
+
+  render() {
+    const { tvShows } = this.props
+    return(
+      <div className={styles.container}>
+        { tvShows.length === 0 && <div> There are now Shows for that search </div>}
+        { tvShows.length !== 0 && tvShows.map((tvShow,index ) => {
+          return (<TvShow key={index} tvShow={tvShow} />)
+        })}
+      </div>
+    )
+  }
+}
+
+export default TvList;
