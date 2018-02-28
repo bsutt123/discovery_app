1c15afa68a023c778354e0ee7a52db0e1258edb8
Brady Sutton <bsutt123@gmail.com>
2018-02-28 12:21:36 -0500

    combine new reducers into root reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -3,15 +3,20 @@ import { combineReducers } from 'redux';
 
 import movies from './moviesReducer';
 import moviesFilter from './moviesFilterReducer';
+import movieDetails from './movieDetailsReducer';
+
 import tv from './tvReducer';
 import tvFilter from './tvFilterReducer';
+import tvDetails from './tvDetailsReducer';
 
 
 const rootReducer = combineReducers({
   movies,
   moviesFilter,
+  movieDetails,
   tv,
-  tvFilter
+  tvFilter,
+  tvDetails
 })
 
 export default rootReducer;
