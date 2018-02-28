6c59eda22f16ed1750e996e8235654b484773051
Brady Sutton <bsutt123@gmail.com>
2018-02-28 12:24:35 -0500

    make the image in tv details into Router Link

--- client/src/components/TvShow/TvShow.jsx
+++ client/src/components/TvShow/TvShow.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styles from './tv-show.module.css';
+import { Link } from 'react-router-dom';
 
 const TvShow = ({ tvShow }) => {
   const prePosterPath = 'https://image.tmdb.org/t/p/w500'
   return(
     <div className={styles.container} >
       <div className={styles.imageBox} >
-        <img src={`${prePosterPath}${tvShow.poster_path}`} alt={`poster for ${tvShow.title}`}className={styles.image} />
+        <Link to={`/tv/${tvShow.id}`} > 
+          <img src={`${prePosterPath}${tvShow.poster_path}`} alt={`poster for ${tvShow.title}`} className={styles.image} /> 
+        </Link>
       </div>
       <div className={styles.contentBox} >
         <h1 className={styles.title} > { tvShow.name } </h1>
