08a48f763939a63bf72d4dc675010072b2fee61c
Brady Sutton <bsutt123@gmail.com>
2018-02-27 22:44:27 -0500

    add tv filters to the root reducer

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -3,11 +3,15 @@ import { combineReducers } from 'redux';
 
 import movies from './moviesReducer';
 import moviesFilter from './moviesFilterReducer';
+import tv from './tvReducer';
+import tvFilter from './tvFilterReducer';
 
 
 const rootReducer = combineReducers({
   movies,
-  moviesFilter
+  moviesFilter,
+  tv,
+  tvFilter
 })
 
 export default rootReducer;
