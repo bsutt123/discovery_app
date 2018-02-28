7e0429b2008c7c05a9425e848f4cd1c2e44a2afe
Brady Sutton <bsutt123@gmail.com>
2018-02-28 12:20:25 -0500

    add the new tv show action to tv action redux

--- client/src/actions/tvActions.js
+++ client/src/actions/tvActions.js
@@ -3,7 +3,7 @@ import request from 'superagent';
 //export action types
 export const SET_TV_FILTER = 'SET_TV_FILTER';
 export const SEARCH_TV = 'SEARCH_TV';
-
+export const SET_TV_DETAILS = 'SET_TV_DETAILS';
 //create actionEmittors
 function updateTvByFilter(filter, tv) {
   return {
@@ -20,6 +20,13 @@ function updateTvBySearch(query, tv) {
   };
 }
 
+function updateTvDetails(tv) {
+  return {
+    type: SET_TV_DETAILS,
+    tv
+  }
+}
+
 //use functions to make calls to the api
 function fetchTvByFilter(filter) {
   return request.get('/api/tv/filter').query({ filter }).then(res => JSON.parse(res.text).results)
@@ -27,6 +34,10 @@ function fetchTvByFilter(filter) {
 
 function fetchTvBySearch(query) {
   return request.get('/api/tv/search').query({ query }).then(res => JSON.parse(res.text).results)
+}
+
+function fetchTvDetails(id) {
+  return request.get(`/api/tv/${id}`).then(res => JSON.parse(res.text))
 }
 
 //combine 2 in a thunk
@@ -43,6 +54,15 @@ export function setTvBySearch(query) {
   return function (dispatch) {
     return fetchTvBySearch(query).then(
       tv => dispatch(updateTvBySearch(query, tv)),
+      error =>  console.log(error)
+    )
+  }
+}
+
+export function setTvDetails(id) {
+  return function (dispatch) {
+    return fetchTvDetails(id).then(
+      tv => dispatch(updateTvDetails(tv)),
       error =>  console.log(error)
     )
   }
