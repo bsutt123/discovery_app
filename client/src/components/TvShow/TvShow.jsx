8e00a8c321766c4d21796a3804720fb0308a14ab
Brady Sutton <bsutt123@gmail.com>
2018-03-28 14:07:26 -0400

    add poster path logic to display message with no poster

--- client/src/components/TvShow/TvShow.jsx
+++ client/src/components/TvShow/TvShow.jsx
@@ -8,7 +8,13 @@ const TvShow = ({ tvShow }) => {
     <div className={styles.container} >
       <div className={styles.imageBox} >
         <Link to={`/tv/${tvShow.id}`} > 
-          <img src={`${prePosterPath}${tvShow.poster_path}`} alt={`poster for ${tvShow.title}`} className={styles.image} /> 
+        {
+          (tvShow.poster_path) && <img src={`${prePosterPath}${tvShow.poster_path}`} alt={`poster for ${tvShow.title}`} className={styles.image}/>
+        }
+        {
+          (!tvShow.poster_path) && <div> No poster for this show, but you can click here to visit the details page</div>
+        }
+            
         </Link>
       </div>
       <div className={styles.contentBox} >
