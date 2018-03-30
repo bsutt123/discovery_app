8a8cb87d38a451dcdc19dc25b4adea07c063970d
Brady <bsutt123@gmail.com>
2018-03-30 18:06:19 -0400

    add SET_TV_PAGE to tv reducer

--- client/src/reducers/tvReducer.js
+++ client/src/reducers/tvReducer.js
@@ -1,10 +1,12 @@
-import { SEARCH_TV, SET_TV_FILTER } from 'Actions/tvActions';
+import { SEARCH_TV, SET_TV_FILTER, SET_TV_PAGE } from 'Actions/tvActions';
 
 export default function tvReducer(state = [], action) {
   switch (action.type) {
     case SEARCH_TV:
       return action.tv
     case SET_TV_FILTER:
+      return action.tv
+    case SET_TV_PAGE: 
       return action.tv
     default:
       return state
