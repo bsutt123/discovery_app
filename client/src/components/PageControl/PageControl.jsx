73db3963105ad8c1802f45163f15dcdd8e702970
Brady <bsutt123@gmail.com>
2018-03-30 18:09:06 -0400

    change to Angles and Double Angles for page control

--- client/src/components/PageControl/PageControl.jsx
+++ client/src/components/PageControl/PageControl.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import styles from './page-control.module.css';
 
-import ArrowCircleLeft from 'react-icons/lib/fa/arrow-circle-left';
-import ArrowCircleRight from 'react-icons/lib/fa/arrow-circle-right';
+import AngleLeft from 'react-icons/lib/fa/angle-left';
+import AngleRight from 'react-icons/lib/fa/angle-right';
+import DoubleAngleLeft from 'react-icons/lib/fa/angle-double-left';
+import DoubleAngleRight from 'react-icons/lib/fa/angle-double-right';
 
-const PageControl = () => {
+const PageControl = ({page, setPage}) => {
   return (
     <div className={styles.container}>
-      <ArrowCircleLeft />
-      <ArrowCircleRight />
+      {page !== 1 &&
+        (
+          <span>
+            <DoubleAngleLeft onClick={(e) => setPage(1, e)} />
+            <AngleLeft onClick={(e) => setPage((page-1), e)} />
+          </span>
+        )
+      }
+      <span> Page Control </span>
+      {
+        page !== 1000 &&
+        (
+          <span>
+            <AngleRight onClick={(e) => setPage((page+1), e)} />
+            <DoubleAngleRight onClick={(e) => setPage(1000, e)} />
+          </span>
+        )
+      }
     </div>
   )
 };
