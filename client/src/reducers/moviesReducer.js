fbea6477b9e2e7c29da83c7bcf630d2abf5d373c
Brady <bsutt123@gmail.com>
2018-03-30 18:05:24 -0400

    add SET_MOVIES_PAGE to movies reducer

--- client/src/reducers/moviesReducer.js
+++ client/src/reducers/moviesReducer.js
@@ -1,4 +1,4 @@
-import { SET_MOVIES_FILTER, SEARCH_MOVIES } from 'Actions/movieActions'
+import { SET_MOVIES_FILTER, SEARCH_MOVIES, SET_MOVIES_PAGE } from 'Actions/movieActions'
 
 
 export function moviesReducer(state = [], action) {
@@ -6,6 +6,8 @@ export function moviesReducer(state = [], action) {
     case SET_MOVIES_FILTER:
       return action.movies
     case SEARCH_MOVIES:
+      return action.movies
+    case SET_MOVIES_PAGE:
       return action.movies
     default:
       return state
